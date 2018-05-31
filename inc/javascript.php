fa55e8f5994131a0aac9a5c02d1cb7e5099de24e
Joshua McNabb <791799@ACDM.DS.SAIT.CA>
2018-05-31 15:05:54 -0600

    Create files.

---
+++ inc/javascript.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js"></script>
