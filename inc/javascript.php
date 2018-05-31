2a7a1d2ca3c1634b17a6113fbefe3839e4dae2d5
Joshua McNabb <791799@ACDM.DS.SAIT.CA>
2018-05-31 15:05:28 -0600

    .
    .

--- inc/javascript.php
+++
@@ -1,2 +0,0 @@
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js"></script>
