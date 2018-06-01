489a5e56461eaf648b2f291400610ac010df4edf
joshuam1995 <joshuam1995@users.noreply.github.com>
2018-05-31 21:28:09 -0600

    Add database configuration with PDO.

---
+++ inc/database.php
@@ -0,0 +1,16 @@
+<?php
+    // Database connection details with PDO
+    $host = '127.0.0.1';
+    $db   = 'travelexpertsgroup';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+    $database = new PDO($dsn, $user, $pass, $opt);
