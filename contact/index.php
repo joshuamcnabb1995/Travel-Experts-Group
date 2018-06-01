f8edb6eced6ee4271bda32403cca4d5275fe0628
sara <Seyedeh-Sara.Nejad-Hashemi@edu.sait.ca>
2018-06-01 14:24:06 -0600

    mod16

--- contact/index.php
+++ contact/index.php
@@ -40,6 +40,22 @@
   ...
 
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321303.3268281848!2d-114.35433369398585!3d51.01278199569303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C+AB!5e0!3m2!1sen!2sca!4v1527883694526" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <pre> Contact AirlineTicketCentre .Ca
+                      Canada Toll-free number: 1.888.289.6660
+                      Worldwide Contact: +1.403.289.6656
+                      Email: fly@travelexpert.ca
+
+                      Locations
+
+                      Calgary
+                      CANADA WIDE CALL CENTRE
+                      Unit B, 916 16th Ave. NW
+                      Calgary, Alberta – T2M 0K3
+
+                      Hours (Mtn Std time):
+                      Mon to Fri : 8am – 6:30pm
+                      Sat : 9am – 4pm
+                      Sun : 11am – 4pm</pre>
                 </div>
     </body>
 </html>
