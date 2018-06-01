47ed664be9ff082bd3e73aa54cde9ec32339d092
joshuam1995 <joshuam1995@users.noreply.github.com>
2018-05-31 21:30:21 -0600

    Add packages page.

---
+++ packages/index.php
@@ -0,0 +1,23 @@
+<?php
+    $page = 2;
+    include('../inc/global.php');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Travel Experts - YOUR PAGE TITLE</title>
+        <?php include('../inc/css.php'); ?>
+    </head>
+
+    <body>
+        <?php include('../inc/navigation.php'); ?>
+        <?php include('../inc/header.php'); ?>
+
+        <div class="container">
+            <!-- Your HTML here -->
+        </div>
+
+        <?php include('../inc/javascript.php'); ?>
+        <?php include('../inc/footer.php'); ?>
+    </body>
+</html>
