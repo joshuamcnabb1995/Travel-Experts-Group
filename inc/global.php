de84243fd2d685efe4da19a077a900efd9fcad0b
joshuam1995 <joshuam1995@users.noreply.github.com>
2018-05-31 21:26:54 -0600

    Add comment.

--- inc/global.php
+++ inc/global.php
@@ -5,3 +5,4 @@
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
+    /* Disable cache temporarily */
