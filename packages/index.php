e88a70c4a83ceec61afa72f112fe29342a379758
Joshua McNabb <791799@ACDM.DS.SAIT.CA>
2018-06-04 15:09:22 -0600

    .

--- packages/index.php
+++ packages/index.php
@@ -20,7 +20,7 @@
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="card mb-4 box-shadow">
-                                    <a href="#"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package1.jpg" data-holder-rendered="true"></a>
+                                    <a href="package.php?id=1"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package1.jpg" data-holder-rendered="true"></a>
                                     <div class="card-body">
                                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis...</p>
                                         <div class="d-flex justify-content-between align-items-center">
@@ -35,7 +35,7 @@
 
                             <div class="col-md-4">
                                 <div class="card mb-4 box-shadow">
-                                    <a href="#"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package2.jpg" data-holder-rendered="true"></a>
+                                    <a href="package.php?id=1"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package2.jpg" data-holder-rendered="true"></a>
                                     <div class="card-body">
                                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis...</p>
                                         <div class="d-flex justify-content-between align-items-center">
@@ -50,7 +50,7 @@
 
                             <div class="col-md-4">
                                 <div class="card mb-4 box-shadow">
-                                    <a href="#"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package3.jpg" data-holder-rendered="true"></a>
+                                    <a href="package.php?id=1"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package3.jpg" data-holder-rendered="true"></a>
                                     <div class="card-body">
                                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis...</p>
                                         <div class="d-flex justify-content-between align-items-center">
@@ -67,7 +67,7 @@
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="card mb-4 box-shadow">
-                                    <a href="#"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package1.jpg" data-holder-rendered="true"></a>
+                                    <a href="package.php?id=1"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package1.jpg" data-holder-rendered="true"></a>
                                     <div class="card-body">
                                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis...</p>
                                         <div class="d-flex justify-content-between align-items-center">
@@ -82,7 +82,7 @@
 
                             <div class="col-md-4">
                                 <div class="card mb-4 box-shadow">
-                                    <a href="#"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package2.jpg" data-holder-rendered="true"></a>
+                                    <a href="package.php?id=1"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package2.jpg" data-holder-rendered="true"></a>
                                     <div class="card-body">
                                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis...</p>
                                         <div class="d-flex justify-content-between align-items-center">
@@ -97,7 +97,7 @@
 
                             <div class="col-md-4">
                                 <div class="card mb-4 box-shadow">
-                                    <a href="#"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package3.jpg" data-holder-rendered="true"></a>
+                                    <a href="package.php?id=1"><img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="../img/packages/package3.jpg" data-holder-rendered="true"></a>
                                     <div class="card-body">
                                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis...</p>
                                         <div class="d-flex justify-content-between align-items-center">
