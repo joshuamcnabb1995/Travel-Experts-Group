ef424f0de30a244cacfc75662b5d81b3fa5007e6
sara <Seyedeh-Sara.Nejad-Hashemi@edu.sait.ca>
2018-06-05 15:42:19 -0600

    .

--- contact/contact1.php
+++ contact/contact1.php
@@ -42,6 +42,20 @@
                 </form>
                 <div class="row">
                   <div class="col-8"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321303.3268281848!2d-114.35433369398585!3d51.01278199569303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C+AB!5e0!3m2!1sen!2sca!4v1527883694526" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+                  <div class="col-4">
+
+                    <?php
+                        include('../inc/database.php');
+
+                        $sql = $database->query("SELECT AgencyId, AgncyAddress, AgncyProv, AgncyCity, AgncyPostal, AgncyCountry, AgncyPhone,AgncyFax  FROM agencies");
+
+                        foreach($sql as $row) {
+                            echo "<br><h4>Contact:</h4></br><h5>".$row["AgncyCity"]."</h5></br>Address: ".$row["AgncyAddress"]."</br>Postalcode: ".$row["AgncyPostal"]."</br>Phone: ".$row["AgncyPhone"]."</br>Fax: ".$row["AgncyFax"]."</br>";
+                        }
+                    ?>
+
+
+                </div>
 
               </div>
         </body>
