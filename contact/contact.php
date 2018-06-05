df823f620465eb6ac506eb3ff9c5e976759f168f
sara <Seyedeh-Sara.Nejad-Hashemi@edu.sait.ca>
2018-06-05 15:01:19 -0600

    .

--- contact/contact.php
+++ contact/contact.php
@@ -1,6 +1,6 @@
 <?php
 if ($_POST["email"]<>'') {
-    $ToEmail = 'sara_n_h@yahoo.com';
+    $ToEmail = "sara_n_h@yahoo.com";
     $EmailSubject = 'Site contact form';
     $mailheader = "From: ".$_POST["email"]."\r\n";
     $mailheader .= "Reply-To: ".$_POST["email"]."\r\n";
@@ -10,7 +10,7 @@ if ($_POST["email"]<>'') {
     $MESSAGE_BODY = "Name: ".$_POST["phone"]."";
     $MESSAGE_BODY .= "Email: ".$_POST["email"]."";
     $MESSAGE_BODY .= "Comment: ".nl2br($_POST["message"])."";
-    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
 ?>
 Your message was sent
 <?php
