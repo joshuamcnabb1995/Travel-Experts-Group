2f360477cae9a0fd4c452fc5d473db58715b0518
joshuam1995 <joshuam1995@users.noreply.github.com>
2018-06-04 20:55:09 -0600

    .

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,43 @@
-<footer class="footer">
+<section class="footer">
     <div class="container">
-        <span class="text-muted">Copyright &copy; 2018 Travel Experts Inc. All rights reserved.</span>
-     </div>
-</footer>
+    	<div class="row">
+        	<div class="col-lg-4  col-md-4 col-sm-4">
+            	<div class="footer_dv">
+                	<h4>Links</h4>
+                	<ul>
+                        <li><a href="#">Link 1</a></li>
+                        <li><a href="#">Link 2</a></li>
+                        <li><a href="#">Link 3</a></li>
+                        <li><a href="#">Link 4</a></li>
+                        <li><a href="#">Link 5</a></li>
+                        <li><a href="#">Link 5</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4  col-md-4 col-sm-4">
+            	<div class="footer_dv">
+                	<h4>Links</h4>
+                	<ul>
+                        <li><a href="#">Link 1</a></li>
+                        <li><a href="#">Link 2</a></li>
+                        <li><a href="#">Link 3</a></li>
+                        <li><a href="#">Link 4</a></li>
+                        <li><a href="#">Link 5</a></li>
+                        <li><a href="#">Link 5</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4  col-md-4 col-sm-4">
+            	<div class="footer_dv">
+                	<h4>Contact us</h4>
+                	<h5>Travel Experts Inc.</h5>
+      			    <p>
+                        1155-8th Ave SW<br />
+                        Calgary, AB &bull; T2P 1N3<br />
+                        1 (403) 271-9873
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
