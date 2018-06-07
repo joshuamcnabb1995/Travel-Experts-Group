45a6605524179846c0c28400725729d193c9bfe9
Seyedeh-Sara Nejad-Hashemi <764974@ACDM.DS.SAIT.CA>
2018-06-07 09:44:27 -0600

    .

--- contact/index.php
+++ contact/index.php
@@ -19,7 +19,7 @@
 
                         <p class="lead">Please send us a message and one of our agents will get in touch with you as soon as possible.</p>
 
-
+                                <!--------------the form-->
                                 <form id="contact-form" method="post" action="contact.php" role="form">
 
                                   <label for="form_name">Firstname *</label>
@@ -44,22 +44,23 @@
                                 </form>
                         </div>
 
-
-                      <div class="col"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321303.3268281848!2d-114.35433369398585!3d51.01278199569303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C+AB!5e0!3m2!1sen!2sca!4v1527883694526" width="350" height="550" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+                          <!-----the map-->
+                        <div class="col"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321303.3268281848!2d-114.35433369398585!3d51.01278199569303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C+AB!5e0!3m2!1sen!2sca!4v1527883694526" width="350" height="550" frameborder="0" style="border:0" allowfullscreen></iframe></div>
                         <div class="col">
 
-                          <?php
-                            include('../inc/database.php');
+                            <!---contact info-->
+                            <?php
+                              include('../inc/database.php');
 
-                            $sql = $database->query("SELECT AgencyId, AgncyAddress, AgncyProv, AgncyCity, AgncyPostal, AgncyCountry, AgncyPhone,AgncyFax  FROM agencies");
+                              $sql = $database->query("SELECT AgencyId, AgncyAddress, AgncyProv, AgncyCity, AgncyPostal, AgncyCountry, AgncyPhone,AgncyFax  FROM agencies");
 
-                            foreach($sql as $row) {
-                              echo "<br><h4>Contact:</h4></br><h5>".$row["AgncyCity"]."</h5></br>Address: ".$row["AgncyAddress"]."</br>Postalcode: ".$row["AgncyPostal"]."</br>Phone: ".$row["AgncyPhone"]."</br>Fax: ".$row["AgncyFax"]."</br>E-mail: info@travelexpert.com</br> ";
-                              }
-                          ?>
+                              foreach($sql as $row) {
+                                echo "<br><h4>Contact:</h4></br><h5>".$row["AgncyCity"]."</h5></br>Address: ".$row["AgncyAddress"]."</br>Postalcode: ".$row["AgncyPostal"]."</br>Phone: ".$row["AgncyPhone"]."</br>Fax: ".$row["AgncyFax"]."</br>E-mail: info@travelexpert.com</br> ";
+                                }
+                            ?>
 
-                    </div>
-              </div>
+                        </div>
+                      </div>
 
         </body>
 </html>
