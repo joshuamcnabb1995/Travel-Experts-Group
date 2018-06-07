72f0a3f81f4fa1091d995ca5ed28b6384c4dff25
Joshua McNabb <791799@ACDM.DS.SAIT.CA>
2018-06-07 10:58:51 -0600

    .

--- register/processRegistration.php
+++ register/processRegistration.php
@@ -3,77 +3,87 @@
         Author: Safiq Momin
         Date of creation: 06-01-2018
         Description: The file contains html code for registration and php code to verify different conditions of the fields relating the database
+        Work Done: Added validation for email, username, password and confirm password fields
 
         Modified by: Joshua McNabb
-        Date: 06-05-2018
-        Description: Converted MySQLi to PDO
+        Date: 06-05-2018 - 06-07-2018
+        Work Done: Converted MySQLi to PDO
                      Added sessions with the text of the error messages
+                     Added additional validation for all form fields
     */
 
     include('../inc/global.php'); // Database configuration and other checks
 
-    $registrationForm = $_POST;
+    $registrationForm = $_POST; // Store all the elements in the form as a variable so they can be looped through
+    $formErrors = 0; // By default, there's no form errors, but they can be added
+
+    // Function to handle error creation to reduce repetition
+    public function generateError($field, $errorMessage)
+    {
+        $_SESSION[$field . 'Error'] = $errorMessage;
+        $formErrors += 1;
+    }
 
     // Loop through all the form elements and save them as variables and sessions
-    foreach($_POST as $key => $value) {
+    foreach($registrationForm as $key => $value) {
         $$key = $value;
-        $_SESSION[$key] = $value;
+        $_SESSION[$key] = trim($value); // Remove whitespace from beginning and end of the form input
     }
 
-    /*$username = trim($_POST['username']);
-    $_SESSION['username'] = $username; // Store the username as a session so it can be put in the input if the user enters it incorrectly
-    $password = $_POST['password'];
-    $_SESSION['password'] = $password;
-    $confirm = $_POST['confirm'];
-    $_SESSION['confirm'] = $confirm;*/
+    // Validate Email
+    if(empty($_POST['email'])) generateError('email', 'Please enter an email address.');
+    else if(!preg_match('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email))
+        generateError('email', 'Invalid email format.');
+    else {
+        $getEmail = $database->prepare("SELECT id FROM customers WHERE CustEmail = ?"); // Prepare a select statement
+        $getEmail->execute([$email]); // Execute the prepared statement with the placeholder
+
+        // Email address belongs to another customer
+        if($getEmail->rowCount() == 1) generateError('email', 'Email address already in use.');
+    }
 
     // Validate username
-    if(empty(trim($_POST['username']))) {
-        $_SESSION['usernameError'] = 'Please enter a username.'; // Save the error message in a session so it can be displayed on the registration page
-        header('Location: index.php');
-    }
-
+    if(empty($_POST['username'])) generateError('username', 'Please enter a username.');
+    else if(strlen($username) < 2) generateError('username', 'Username must be between 2 and 25 characters long.');
+    else if(strlen($username) > 25) generateError('username', 'Username must be between 2 and 25 characters long.');
     else {
-        $getUser = $database->prepare("SELECT id FROM users WHERE username = ?"); // Prepare a select statement
-        $getUser->execute([$username]); // Execute the prepared statement with the placeholder
+        $getUser = $database->prepare("SELECT id FROM users WHERE username = ?");
+        $getUser->execute([$username]);
 
-        if($getUser->rowCount() == 1) {
-            $_SESSION['usernameError'] = 'This username is taken.';
-            header('Location: index.php');
-        }
+        if($getUser->rowCount() == 1) generateError('username', 'This username is taken.');
     }
+    // Validate username
 
     // Validating the password
-    if(empty(trim($_POST['password']))) {
+    if(empty($_POST['password'])) {
         $_SESSION['passwordError'] = 'Please enter a password.';
-        header('Location: index.php');
+        $formErrors += 1;
     }
 
-    else if(strlen(trim($_POST['password'])) < 6) {
+    else if(strlen($_POST['password']) < 6) {
         $_SESSION['passwordError'] = 'Password must have atleast 6 characters.';
-        header('Location: index.php');
+        $formErrors += 1;
     }
 
     else
-        $password = trim($_POST['password']);
+        $password = $_POST['password'];
 
     // Validate confirm password
-    if(empty(trim($_POST['confirm']))) {
+    if(empty($_POST['confirm'])) {
         $_SESSION['confirmError'] = 'Please confirm password.';
-        header('Location: index.php');
+        $formErrors += 1;
     }
 
     else {
-        $confirm_password = trim($_POST['confirm']);
+        $confirm_password = $_POST['confirm'];
         if($password != $confirm_password) {
-            $_SESSION['confirmError'] = 'Password did not match.';
-            header('Location: index.php');
+            $_SESSION['confirmError'] = 'Passwords do not match.';
+            $formErrors += 1;
         }
     }
 
-    // Check input errors before inserting in database
-    if(empty($_SESSION['usernameError']) && empty($_SESSION['passwordError']) && empty($_SESSION['confirmError'])) {
-        // Prepare an insert statement
+    // Check form errors before inserting in database
+    if($formErrors == 0) {
         $addUser = $database->query("INSERT INTO users (username, password) VALUES (?, ?)");
         $passwordOptions = [ 'cost' => 13 ]; // Make the password somewhat difficult to crack
         $addUser->execute($username, password_hash($password, PASSWORD_BCRYPT, $passwordOptions)); // Hash the password with Bcrypt so it's more secure than a plaintext password
@@ -83,4 +93,9 @@
 
         else
             echo 'Something went wrong. Please try again later.'; // Otherwise, show an error
+    }
+
+    else {
+        $_SESSION['formErrors'] = $formErrors;
+        header('Location: index.php');
     }
