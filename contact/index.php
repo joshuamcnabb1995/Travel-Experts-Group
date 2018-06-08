3ddd2461c386930993a7626c4c8e2752914d1446
Seyedeh-Sara Nejad-Hashemi <764974@ACDM.DS.SAIT.CA>
2018-06-08 11:04:27 -0600

    .

--- contact/index.php
+++ contact/index.php
@@ -1,11 +1,8 @@
-<?php $page = 3; ?>
 <html>
     <head>
         <title>Contact Form</title>
         <?php include('../inc/css.php'); ?>
         <style>
-        .col-8{ background: rgb(29, 198, 193); }
-        .col-4{ background-color: rgb(129, 212, 165);}
         .col-5{ background:rgb(208, 242, 233); }
         .col{ background-color: rgb(129, 212, 165);}
         </style>
@@ -13,6 +10,8 @@
     <body>
         <?php include('../inc/navigation.php'); ?>
         <div class="container" style="margin-top:150px;margin-bottom:100px">
+          <?php include('../inc/javascript.php'); ?>
+          <?php include('../inc/footer.php'); ?>
                 <div class="container">
                   <div class="row">
                     <div class="col-5">
@@ -39,41 +38,12 @@
                                   <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea></br>
                                   <div class="help-block with-errors"></div>
 
-                    <label for="form_message">Message *</label>
-                    <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea></br>
-                    <div class="help-block with-errors"></div>
                                   <input type="submit" class="btn btn-success btn-send" value="Send message">
 
                                   <p class="text-muted"><strong>*</strong> These fields are required.</p></br>
                                 </form>
                         </div>
 
-                <p class="text-muted"><strong>*</strong> These fields are required.</p></br>
-                </form>
-                <div class="row">
-                  <div class="col-8"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321303.3268281848!2d-114.35433369398585!3d51.01278199569303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C+AB!5e0!3m2!1sen!2sca!4v1527883694526" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
-                  <div class="col-4">
-                    <pre>
-                      <h3>Contact:</h3>
-                      travelexpert.Ca
-                      Canada Toll-free number: 1.888.289.6660
-                      Worldwide Contact: +1.403.289.6656
-                      Email: fly@travelexpert.ca
-
-                      <h3>Locations:</h3>
-
-                      Calgary                         Okotoks
-                      CANADA WIDE CALL CENTRE
-                      Unit B, 916 16th Ave            7th St
-                      Calgary, Alberta T2M 0K3        Okotoks, Alberta V1c6M1
-
-                      Hours (Mtn Std time):
-                      Mon to Fri : 8am – 6:30pm
-                      Sat : 9am – 4pm
-                      Sun : 11am – 4pm
-                </pre>
-                </div>
-              </div>
                           <!-----the map-->
                         <div class="col"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321303.3268281848!2d-114.35433369398585!3d51.01278199569303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C+AB!5e0!3m2!1sen!2sca!4v1527883694526" width="350" height="550" frameborder="0" style="border:0" allowfullscreen></iframe></div>
                         <div class="col">
@@ -91,8 +61,6 @@
 
                         </div>
                       </div>
-					  </div></div>
-					  <?php include('../inc/javascript.php'); ?>
-          <?php include('../inc/footer.php'); ?>
+
         </body>
 </html>
