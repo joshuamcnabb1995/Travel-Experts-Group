cce729621cf13e6ef6c525846443b6e84a397c38
Seyedeh-Sara Nejad-Hashemi <764974@ACDM.DS.SAIT.CA>
2018-06-08 10:58:38 -0600

    .

--- contact/index.php
+++ contact/index.php
@@ -1,3 +1,4 @@
+<?php $page = 3; ?>
 <html>
     <head>
         <title>Contact Form</title>
@@ -12,8 +13,6 @@
     <body>
         <?php include('../inc/navigation.php'); ?>
         <div class="container" style="margin-top:150px;margin-bottom:100px">
-          <?php include('../inc/javascript.php'); ?>
-          <?php include('../inc/footer.php'); ?>
                 <div class="container">
                   <div class="row">
                     <div class="col-5">
@@ -40,19 +39,15 @@
                                   <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea></br>
                                   <div class="help-block with-errors"></div>
 
-<<<<<<< HEAD
                     <label for="form_message">Message *</label>
                     <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea></br>
                     <div class="help-block with-errors"></div>
-=======
                                   <input type="submit" class="btn btn-success btn-send" value="Send message">
->>>>>>> Sara
 
                                   <p class="text-muted"><strong>*</strong> These fields are required.</p></br>
                                 </form>
                         </div>
 
-<<<<<<< HEAD
                 <p class="text-muted"><strong>*</strong> These fields are required.</p></br>
                 </form>
                 <div class="row">
@@ -79,7 +74,6 @@
                 </pre>
                 </div>
               </div>
-=======
                           <!-----the map-->
                         <div class="col"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321303.3268281848!2d-114.35433369398585!3d51.01278199569303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C+AB!5e0!3m2!1sen!2sca!4v1527883694526" width="350" height="550" frameborder="0" style="border:0" allowfullscreen></iframe></div>
                         <div class="col">
@@ -97,7 +91,8 @@
 
                         </div>
                       </div>
-
->>>>>>> Sara
+					  </div></div>
+					  <?php include('../inc/javascript.php'); ?>
+          <?php include('../inc/footer.php'); ?>
         </body>
 </html>
